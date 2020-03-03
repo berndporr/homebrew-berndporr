7f44680548fd9473925aa667fe3e185722ffae33
Bernd Porr <bernd@glasgowneuro.tech>
2020-03-03 11:22:20 +0000

    Added the 1st formula for the iir filter

---
+++ iir.rb
@@ -0,0 +1,18 @@
+class Iir < Formula
+  desc "IIR realtime filter library written in C++"
+  url "https://github.com/berndporr/iir1/archive/1.7.7.zip"
+  homepage "https://github.com/berndporr/iir1"
+  sha256 "e9ebeea7e6e109f94654d3117dbb1c6c758ed9406be537ecef7f888ea1095c79"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "ctest"
+  end
+end
