caf7cf404962fa8143ccbd642fb95301fbaf4451
Bernd Porr <bernd@glasgowneuro.tech>
2021-01-16 08:55:24 +0000

    New iir filter version

--- iir.rb
+++ iir.rb
@@ -1,8 +1,7 @@
 class Iir < Formula
   desc "IIR realtime filter library written in C++"
-  url "https://github.com/berndporr/iir1/archive/1.7.9.zip"
+  url "https://github.com/berndporr/iir1/archive/1.8.0.zip"
   homepage "https://github.com/berndporr/iir1"
-  sha256 "3e2b0a38214a71889d3231145892ca9317b2802de5b3f6281a374c61e57a1618"
   depends_on "cmake" => :build
 
   def install
