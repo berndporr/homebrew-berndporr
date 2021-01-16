805223ddfb66fb463612550321797cc916e0e157
Bernd Porr <bernd@glasgowneuro.tech>
2021-01-16 09:03:49 +0000

    Added checksum

--- iir.rb
+++ iir.rb
@@ -3,6 +3,7 @@ class Iir < Formula
   url "https://github.com/berndporr/iir1/archive/1.8.0.zip"
   homepage "https://github.com/berndporr/iir1"
   depends_on "cmake" => :build
+  sha256 "6d150de04cef4dcd9914819a7947c52e54b33de0bd5777adad4067bcadbf4430"
 
   def install
     system "cmake", ".", *std_cmake_args
