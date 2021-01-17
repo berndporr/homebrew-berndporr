f4a3b60d51d32b08d52533ea142cdef998017931
Bernd Porr <bernd@glasgowneuro.tech>
2021-01-17 13:20:19 +0000

    Added the sha

--- fir.rb
+++ fir.rb
@@ -2,6 +2,7 @@ class Fir < Formula
   desc "FIR filter implementation with LMS in C++"
   homepage "https://github.com/berndporr/fir1"
   url "https://github.com/berndporr/fir1/archive/1.5.0.tar.gz"
+  sha256 "f0d3cbbe4128fa38cf69bcfa174f059c8e5e3666eea2cde7efe76af189cda335"
 
   depends_on "cmake" => :build
 
