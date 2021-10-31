6082d6b43e9d544763e3cf8355d2ec747bd5dd76
Bernd Porr <bernd@glasgowneuro.tech>
2021-10-31 09:46:33 +0000

    Added checksum

--- fir.rb
+++ fir.rb
@@ -2,7 +2,7 @@ class Fir < Formula
   desc "FIR filter implementation with LMS in C++"
   homepage "https://github.com/berndporr/fir1"
   url "https://github.com/berndporr/fir1/archive/1.7.1.tar.gz"
-#  sha256 "f0d3cbbe4128fa38cf69bcfa174f059c8e5e3666eea2cde7efe76af189cda335"
+  sha256 "310d18dc37507da7f7675a55aab7a85386d9ccc66b5cf137e1eb71d6ad7ec7a7"
 
   depends_on "cmake" => :build
 
