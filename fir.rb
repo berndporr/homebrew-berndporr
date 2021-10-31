464f643a7972658597323d517490180d4bc867b7
Bernd Porr <bernd@glasgowneuro.tech>
2021-10-31 09:44:17 +0000

    New fir verson

--- fir.rb
+++ fir.rb
@@ -1,8 +1,8 @@
 class Fir < Formula
   desc "FIR filter implementation with LMS in C++"
   homepage "https://github.com/berndporr/fir1"
-  url "https://github.com/berndporr/fir1/archive/1.5.0.tar.gz"
-  sha256 "f0d3cbbe4128fa38cf69bcfa174f059c8e5e3666eea2cde7efe76af189cda335"
+  url "https://github.com/berndporr/fir1/archive/1.7.1.tar.gz"
+#  sha256 "f0d3cbbe4128fa38cf69bcfa174f059c8e5e3666eea2cde7efe76af189cda335"
 
   depends_on "cmake" => :build
 
