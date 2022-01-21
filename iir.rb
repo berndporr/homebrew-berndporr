e7213c0405080958b0a66a4f887e042ae9b32de9
Bernd Porr <bernd@glasgowneuro.tech>
2022-01-21 19:33:35 +0000

    Added hash

--- iir.rb
+++ iir.rb
@@ -3,8 +3,7 @@ class Iir < Formula
   url "https://github.com/berndporr/iir1/archive/1.9.1.zip"
   homepage "https://github.com/berndporr/iir1"
   depends_on "cmake" => :build
-#  sha256 "eb2512838ced9aa100e279dca96cc0a84e47672e57ec23c67ca56b18848c27ed"
-
+  sha256 "9e202e99764f8b25d43442c429519725c487a5358c262a2b81bbc17a9d79eac0"
   def install
     system "cmake", ".", *std_cmake_args
     system "make"
